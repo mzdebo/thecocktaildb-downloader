3e6a934ebfbfc86b14e82989fb0cbca822a48821
mzdebo <ccherie@ccherie.me>
2020-07-31 10:19:37 -0400

    Update drinks.js

--- drinks.js
+++ drinks.js
@@ -1,5 +1,4 @@
-{
-		"idDrink": "11000",
+{"idDrink": "11000",
 		"strDrink": "Mojito",
 		"strDrinkAlternate": null,
 		"strDrinkES": null,
@@ -31047,5 +31046,4 @@
 		"strMeasure14": null,
 		"strMeasure15": null,
 		"strCreativeCommonsConfirmed": "No",
-		"dateModified": "2017-09-07 21:44:05"
-	}
+		"dateModified": "2017-09-07 21:44:05"}
